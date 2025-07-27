30e788700522e932d90d0a17f5590e7908fa2f19
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-27 19:12:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,38 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIO(server);
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-  let currentRoom = null;
-  let currentUser = null;
-
-  socket.on('join-room', (roomId, username) => {
-    currentRoom = roomId;
-    currentUser = username;
-
+io.on('connection', socket => {
+  socket.on('join', ({ username, roomId }) => {
     socket.join(roomId);
-    socket.to(roomId).emit('user-joined', username);
-
-    console.log(`${username} joined room ${roomId}`);
-
-    socket.on('send-message', (message) => {
-      io.to(roomId).emit('receive-message', { message, username });
-    });
-
-    socket.on('incoming-call', () => {
-      socket.to(currentRoom).emit('incoming-call');
-    });
-
-    socket.on('call-accepted', () => {
-      socket.to(currentRoom).emit('call-accepted');
-    });
-
-    socket.on('call-rejected', () => {
-      socket.to(currentRoom).emit('call-rejected');
-    });
-
-    socket.on('offer', (data) => {
-      socket.to(roomId).emit('offer', data);
-    });
-
-    socket.on('answer', (data) => {
-      socket.to(roomId).emit('answer', data);
-    });
-
-    socket.on('ice-candidate', (data) => {
-      socket.to(roomId).emit('ice-candidate', data);
-    });
+    socket.to(roomId).emit('user-joined', { username });
   });
 
-  socket.on('disconnect', () => {
-    if (currentRoom && currentUser) {
-      socket.to(currentRoom).emit('user-left', currentUser);
-      console.log(`${currentUser} disconnected from room ${currentRoom}`);
-    }
+  socket.on('start-call', ({ roomId, from }) => {
+    socket.to(roomId).emit('incoming-call', { from });
+  });
+
+  socket.on('call-accepted', ({ roomId, from }) => {
+    socket.to(roomId).emit('call-accepted');
+  });
+
+  socket.on('offer', ({ offer, roomId }) => {
+    socket.to(roomId).emit('offer', { offer });
+  });
+
+  socket.on('answer', ({ answer, roomId }) => {
+    socket.to(roomId).emit('answer', { answer });
+  });
+
+  socket.on('ice-candidate', ({ candidate, roomId }) => {
+    socket.to(roomId).emit('ice-candidate', { candidate });
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
