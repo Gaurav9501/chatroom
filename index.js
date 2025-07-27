781972dfce575a074f1d3c90ddd4861d11508a91
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-27 12:21:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = new Server(server);
 
 // Serve static files
 app.use(express.static(__dirname));
 
-// Serve index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle socket connections
+// Socket.IO room-based messaging
 io.on('connection', (socket) => {
-    console.log('A user connected');
+  socket.on('join-room', (room) => {
+    socket.join(room);
+  });
 
-    socket.on('join room', (room) => {
-        socket.join(room);
-        console.log(`User joined room: ${room}`);
-    });
-
-    socket.on('chat message', ({ name, room, message }) => {
-        io.to(room).emit('chat message', { name, message });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('chat-message', (data) => {
+    io.to(data.room).emit('chat-message', data);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
