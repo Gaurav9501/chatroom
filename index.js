e9b20680f63183057c3b6f4e4bf0a80dd250baf9
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-28 13:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,66 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// In-memory storage
-const movieList = [
-  { id: 'inception', image: 'https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg' },
-  { id: 'matrix', image: 'https://image.tmdb.org/t/p/w500/aOi8WhZzQZctdVxrQbbpHzxRiXo.jpg' },
-  { id: 'interstellar', image: 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg' }
+const movies = [
+  {
+    id: uuidv4(),
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/9a/Trolls_World_Tour_poster.jpg',
+    votes: 0,
+    uploaded: false
+  },
+  {
+    id: uuidv4(),
+    image: 'https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg',
+    votes: 0,
+    uploaded: false
+  }
 ];
-const voteCounts = {
-  inception: 0,
-  matrix: 0,
-  interstellar: 0
-};
 
-app.use(express.static(__dirname));
-
-app.get('/movies', (req, res) => {
-  res.sendFile(path.join(__dirname, 'movies.html'));
-});
+app.use(express.static(path.join(__dirname)));
 
 io.on('connection', (socket) => {
-  // Chat
-  socket.on('joinRoom', ({ roomId, username }) => socket.join(roomId));
-  socket.on('chat message', ({ roomId, message }) => {
-    io.to(roomId).emit('chat message', message);
-  });
+  socket.emit('updateVotes', movies);
 
-  // Voting
   socket.on('getVotes', () => {
-    socket.emit('initMovies', movieList, voteCounts);
+    socket.emit('updateVotes', movies);
   });
 
   socket.on('voteMovie', (movieId) => {
-    if (voteCounts[movieId] !== undefined) {
-      voteCounts[movieId]++;
-      io.emit('updateVotes', voteCounts);
+    const movie = movies.find(m => m.id === movieId);
+    if (movie) {
+      movie.votes++;
+      io.emit('updateVotes', movies);
     }
   });
 
-  socket.on('addMovie', (base64Image) => {
-    const id = uuidv4();
-    const newMovie = { id, image: base64Image };
-    movieList.push(newMovie);
-    voteCounts[id] = 0;
-    io.emit('newMovie', newMovie);
+  socket.on('addMovie', (imageUrl) => {
+    const newMovie = {
+      id: uuidv4(),
+      image: imageUrl,
+      votes: 0,
+      uploaded: true
+    };
+    movies.push(newMovie);
+    io.emit('updateVotes', movies);
+  });
+
+  socket.on('deleteMovie', (movieId) => {
+    const index = movies.findIndex(m => m.id === movieId && m.uploaded);
+    if (index !== -1) {
+      movies.splice(index, 1);
+      io.emit('updateVotes', movies);
+    }
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
 });
