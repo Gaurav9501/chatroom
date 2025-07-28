6f81a1ca203202d975f2188126b8845711b0412b
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-28 12:51:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,39 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files (like index.html, movies.html)
 app.use(express.static(__dirname));
+
+// In-memory vote storage
+const movieVotes = {
+  inception: 0,
+  matrix: 0,
+  interstellar: 0,
+};
 
 app.get('/movies', (req, res) => {
   res.sendFile(path.join(__dirname, 'movies.html'));
 });
 
 io.on('connection', (socket) => {
+  // Chat functionality
   socket.on('joinRoom', ({ roomId, username }) => {
     socket.join(roomId);
-    console.log(`${username} joined room ${roomId}`);
   });
 
   socket.on('chat message', ({ roomId, message }) => {
     io.to(roomId).emit('chat message', message);
+  });
+
+  // Movie voting
+  socket.on('getVotes', () => {
+    socket.emit('updateVotes', movieVotes);
+  });
+
+  socket.on('voteMovie', (movieId) => {
+    if (movieVotes[movieId] !== undefined) {
+      movieVotes[movieId]++;
+      io.emit('updateVotes', movieVotes); // broadcast to all
+    }
   });
 });
 
