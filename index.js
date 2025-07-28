ddc08facaf06eab8f56fda816fd0df55efe33d17
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-28 11:51:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ const io = new Server(server);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // Broadcast to all users
+  console.log('A user connected');
+
+  socket.on('joinRoom', ({ roomId, username }) => {
+    socket.join(roomId);
+    console.log(`${username} joined room ${roomId}`);
+  });
+
+  socket.on('chat message', ({ roomId, message }) => {
+    io.to(roomId).emit('chat message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
