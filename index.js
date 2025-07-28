1737c926a1387b86f90e03f5326bbbedd01e1ef1
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-28 12:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,55 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
-
-// In-memory vote storage
-const movieVotes = {
+// In-memory storage
+const movieList = [
+  { id: 'inception', image: 'https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg' },
+  { id: 'matrix', image: 'https://image.tmdb.org/t/p/w500/aOi8WhZzQZctdVxrQbbpHzxRiXo.jpg' },
+  { id: 'interstellar', image: 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg' }
+];
+const voteCounts = {
   inception: 0,
   matrix: 0,
-  interstellar: 0,
+  interstellar: 0
 };
+
+app.use(express.static(__dirname));
 
 app.get('/movies', (req, res) => {
   res.sendFile(path.join(__dirname, 'movies.html'));
 });
 
 io.on('connection', (socket) => {
-  // Chat functionality
-  socket.on('joinRoom', ({ roomId, username }) => {
-    socket.join(roomId);
-  });
-
+  // Chat
+  socket.on('joinRoom', ({ roomId, username }) => socket.join(roomId));
   socket.on('chat message', ({ roomId, message }) => {
     io.to(roomId).emit('chat message', message);
   });
 
-  // Movie voting
+  // Voting
   socket.on('getVotes', () => {
-    socket.emit('updateVotes', movieVotes);
+    socket.emit('initMovies', movieList, voteCounts);
   });
 
   socket.on('voteMovie', (movieId) => {
-    if (movieVotes[movieId] !== undefined) {
-      movieVotes[movieId]++;
-      io.emit('updateVotes', movieVotes); // broadcast to all
+    if (voteCounts[movieId] !== undefined) {
+      voteCounts[movieId]++;
+      io.emit('updateVotes', voteCounts);
     }
+  });
+
+  socket.on('addMovie', (base64Image) => {
+    const id = uuidv4();
+    const newMovie = { id, image: base64Image };
+    movieList.push(newMovie);
+    voteCounts[id] = 0;
+    io.emit('newMovie', newMovie);
   });
 });
 
