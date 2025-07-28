a3b1908a2bf5660e8fc76ff765d7cc4ed1866da5
Gaurav Agarwal <67853442+Gaurav9501@users.noreply.github.com>
2025-07-28 12:32:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Serve static files (like index.html, movies.html)
 app.use(express.static(__dirname));
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.get('/movies', (req, res) => {
+  res.sendFile(path.join(__dirname, 'movies.html'));
+});
 
+io.on('connection', (socket) => {
   socket.on('joinRoom', ({ roomId, username }) => {
     socket.join(roomId);
     console.log(`${username} joined room ${roomId}`);
@@ -18,10 +22,6 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', ({ roomId, message }) => {
     io.to(roomId).emit('chat message', message);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
   });
 });
 
